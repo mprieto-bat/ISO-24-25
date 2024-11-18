5c96a66664d15dfeea5a6121d7d565dda062fd32
mprieto-bat <mprietol10@educarex.es>
2024-11-18 09:51:27 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,16 @@
+@echo off
+REM Script MkProfile
+REM Autor: Manuel Prieto 
+REM Fecha: 18/11/24
+
+mkdir %USERPROFILE%\Asir %USERPROFILE%\Daw %USERPROFILE%\Dam %USERPROFILE%\Ceti %USERPROFILE%\Smr
+
+mkdir %USERPROFILE%\Dam\Docs %USERPROFILE%\Dam\Pract %USERPROFILE%\Dam\Exam
+
+mkdir %USERPROFILE%\Daw\Docs %USERPROFILE%\Daw\Pract  %USERPROFILE%\Daw\Exam
+
+mkdir %USERPROFILE%\Smr\Docs %USERPROFILE%\Smr\Pract  %USERPROFILE%\Smr\Exam
+
+mkdir %USERPROFILE%\Ceti\Docs %USERPROFILE%\Ceti\Pract %USERPROFILE%\Ceti\Exam
+
+mkdir %USERPROFILE%\Asir\Docs %USERPROFILE%\Asir\Pract %USERPROFILE%\Asir\Exam
