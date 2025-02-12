ddacc1b3a7d08ff299a59211dd54da3ec2df8df2
mprieto-bat <mprietol10@educarex.es>
2025-02-12 11:00:27 +0100

    FiveOrMore.sh

--- Scripts/FiveOrMore.sh
+++ Scripts/FiveOrMore.sh
@@ -10,7 +10,6 @@ if [ $# -ne 2 ]; then
     exit
 fi
 
-# Verificar si el directorio está vacío
 arch=$(ls -A "$2" | wc -l)
 
 if [ $arch -eq 0 ]; then
@@ -18,22 +17,20 @@ if [ $arch -eq 0 ]; then
     exit
 fi
 
-# Iterar sobre archivos .txt en el directorio especificado
 for i in "$2"/*.txt; do
-    # Contar las líneas del archivo
+  
     lineas=$(wc -l < "$i")
     echo "$lineas"
 
     if [ $lineas -ge 5 ]; then
         echo "$i"
         echo "$i" >> "$1"
-        # Contar palabras en el archivo de salida
+     
         pal=$(wc -w < "$i")
         echo "El fichero original tiene $pal palabras" > "$1.q"
         cat "$i" >> "$i.q"
     fi
 done
 
-# Contar palabras en el archivo de salida final
 num=$(wc -w < "$1")
 echo "El fichero $1 tiene $num palabras" >> "$1"
