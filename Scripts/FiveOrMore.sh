5913ac58eb4f94e473fbcd5a6c7a22e222097734
mprieto-bat <mprietol10@educarex.es>
2025-02-12 10:59:54 +0100

    FiveOrMore.sh

--- Scripts/FiveOrMore.sh
+++ Scripts/FiveOrMore.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 # Autor : Manuel Prieto L
 # Script Five Or More
-# Fehca : 11/02/2025
+# Fecha : 11/02/2025
 
 clear
 
-if [ $# -ne 2 ];then
-
-	echo "Parametros invalidos"
-	exit
+if [ $# -ne 2 ]; then
+    echo "Parámetros inválidos"
+    exit
 fi
 
-arch=$(ls $2 | wc -l)
+# Verificar si el directorio está vacío
+arch=$(ls -A "$2" | wc -l)
 
-if [ $arch -eq 0 ];then
-
-	echo "Directorio $2 vacío"
-	exit
-
+if [ $arch -eq 0 ]; then
+    echo "Directorio $2 vacío"
+    exit
 fi
 
-for i in $2/*.txt;do
+# Iterar sobre archivos .txt en el directorio especificado
+for i in "$2"/*.txt; do
+    # Contar las líneas del archivo
+    lineas=$(wc -l < "$i")
+    echo "$lineas"
 
-	lineas=$( cat $i | wc -l)
-	echo "$lineas"
-
-	if [ $lineas -ge 5 ];then
-	echo $i
-	echo $i >> $1
-	pal=$(cat $1 | wc -w)
-	echo "El fichero original tiene $pal palabras" > $1.q
-	cat $i >> $i.q
-
-	fi
+    if [ $lineas -ge 5 ]; then
+        echo "$i"
+        echo "$i" >> "$1"
+        # Contar palabras en el archivo de salida
+        pal=$(wc -w < "$i")
+        echo "El fichero original tiene $pal palabras" > "$1.q"
+        cat "$i" >> "$i.q"
+    fi
 done
 
-	num=$(cat $1 | wc -w)
-	echo "El fichero $1 tiene $num palabras" >> $1
-
-
+# Contar palabras en el archivo de salida final
+num=$(wc -w < "$1")
+echo "El fichero $1 tiene $num palabras" >> "$1"
